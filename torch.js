90c94c2b02e877f58f186dbe8403ab705d141d86
cocktailpeanut <cocktailpeanuts@proton.me>
2025-03-07 13:03:07 -0800

    RTX 50 series handling

--- torch.js
+++ torch.js
@@ -1,16 +1,31 @@
 module.exports = {
   run: [
-//    // nvidia 50 series (windows/linux)
-//    {
-//      "when": "{{gpu === 'nvidia' && gpu_model && / 50[0-9]+/.test(gpu_model) }}",
-//      "method": "shell.run",
-//      "params": {
-//        "venv": "{{args && args.venv ? args.venv : null}}",
-//        "path": "{{args && args.path ? args.path : '.'}}",
-//        "message": "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
-//      },
-//      "next": null
-//    },
+    // nvidia 50 series (windows)
+    {
+      "when": "{{gpu === 'nvidia' platform === 'win32' && gpu_model && / 50.+/.test(gpu_model) }}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": [
+          "uv pip install https://huggingface.co/w-e-w/torch-2.6.0-cu128.nv/resolve/main/torch-2.6.0%2Bcu128.nv-cp310-cp310-win_amd64.whl",
+          "uv pip install https://huggingface.co/w-e-w/torch-2.6.0-cu128.nv/resolve/main/torchvision-0.20.0a0%2Bcu128.nv-cp310-cp310-win_amd64.whl",
+          "uv pip install https://huggingface.co/DeathDaDev/torchaudio-2.6.0-cu128/resolve/main/torchaudio-2.6.0a0%2Bc670ad8-cp310-cp310-win_amd64.whl",
+        ]
+      },
+      "next": null
+    },
+    // nvidia 50 series (linux)
+    {
+      "when": "{{gpu === 'nvidia' platform === 'linux' && gpu_model && / 50.+/.test(gpu_model) }}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128"
+      },
+      "next": null
+    },
     // windows nvidia
     {
       "when": "{{platform === 'win32' && gpu === 'nvidia'}}",
